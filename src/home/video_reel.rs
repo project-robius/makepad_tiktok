985e10de0cc184136b01a5fa81011da4feefc9d4
Jorge Bejar <jorge@wyeworks.com>
2023-09-07 23:27:42 -0300

    Implement dragging behavior for the video reel

--- src/home/video_reel.rs
+++ src/home/video_reel.rs
@@ -96,6 +96,9 @@ pub struct VideoReel {
     last_abs: f64,
 
     #[rust]
+    init_drag_time: f64,
+
+    #[rust]
     image_containers: Vec<ViewRef>,
 
     #[rust(0)]
@@ -103,6 +106,12 @@ pub struct VideoReel {
 
     #[rust(0)]
     previous_image_index: i32,
+
+    #[rust]
+    dragging: bool,
+
+    #[rust]
+    offset_from_drag: f64,
 
     #[rust(VideoReelDirection::Forward)]
     direction: VideoReelDirection,
@@ -160,11 +169,13 @@ impl Widget for VideoReel {
 impl VideoReel {
     fn control_animation(&mut self, cx: &mut Cx, event: &Event) {
         if let Some(_ne) = self.next_view.is_event(event) {
-            if self.animator_handle_event(cx, event).is_animating() {
-                self.update_image_positions(cx);
-                self.redraw(cx);
-            } else {
-                self.fire_next_animation(cx);
+            if !self.dragging {
+                if self.animator_handle_event(cx, event).is_animating() {
+                    self.update_image_positions(cx);
+                    self.redraw(cx);
+                } else {
+                    self.fire_next_animation(cx);
+                }
             }
             self.next_view = cx.new_next_frame();
         }
@@ -186,16 +197,16 @@ impl VideoReel {
     fn update_image_positions(&mut self, cx: &mut Cx) {
         if self.animator.animator_in_state(cx, id!(carrousel.display)) {
             let direction = self.direction;
-            let offset = self.offset;
-
             let (mut prev_image, mut current_image) = self.get_active_images_containers();
 
             match direction {
                 VideoReelDirection::Forward => {
+                    let offset = (self.offset - self.offset_from_drag).max(0.0);
                     Self::set_vertical_position(&mut current_image, offset, cx);
                     Self::set_vertical_position(&mut prev_image, offset - IMAGE_HEIGHT, cx);
                 },
                 VideoReelDirection::Backward => {
+                    let offset = (self.offset + self.offset_from_drag).max(0.0);
                     Self::set_vertical_position(&mut current_image, -offset, cx);
                     Self::set_vertical_position(&mut prev_image, IMAGE_HEIGHT - offset, cx);
                 }
@@ -220,28 +231,59 @@ impl VideoReel {
     }
 
     fn handle_mouse_event(&mut self, cx: &mut Cx, event: &Event) {
+        if self.animator.is_track_animating(cx, id!(carrousel)) &&
+            self.animator.animator_in_state(cx, id!(carrousel.display)) {
+            return;
+        }
+
         match event.hits(cx, self.view.area()) {
             Hit::FingerDown(fe) => {
                 self.last_abs = fe.abs.y;
-            }
+                self.init_drag_time = fe.time;
+                self.offset_from_drag = 0.0;
+            },
+            Hit::FingerMove(fe) => {
+                let time_elapsed = fe.time - self.init_drag_time;
+               // dbg!(time_elapsed);
+
+                if time_elapsed > 0.15 {
+                    let delta = self.last_abs - fe.abs.y;
+                    let (_, mut current_image) = self.get_active_images_containers();
+
+                    self.dragging = true;
+                    if fe.abs.y > self.last_abs {
+                        let upcoming_image_index = (self.current_image_index - 1).rem_euclid(self.image_containers.len() as i32);
+                        let mut upcoming_image = self.image_containers[upcoming_image_index as usize].clone();
+                        Self::set_vertical_position(&mut upcoming_image, -IMAGE_HEIGHT - delta, cx);
+                        upcoming_image.set_visible(true);
+                    } else {
+                        let upcoming_image_index = (self.current_image_index + 1).rem_euclid(self.image_containers.len() as i32);
+                        let mut upcoming_image = self.image_containers[upcoming_image_index as usize].clone();
+                        Self::set_vertical_position(&mut upcoming_image, IMAGE_HEIGHT - delta, cx);
+                        upcoming_image.set_visible(true);
+                    }
+
+                    self.offset = -delta;
+                    Self::set_vertical_position(&mut current_image, -delta, cx);
+                    self.redraw(cx);
+                }
+
+            },
             Hit::FingerUp(fe) => {
                 if fe.is_over && (fe.abs.y - self.last_abs).abs() > 10.0 {
                     self.previous_image_index = self.current_image_index;
 
-                    let initial_offset = if fe.abs.y > self.last_abs {
+                    if fe.abs.y > self.last_abs {
                         self.setup_next_animation(VideoReelDirection::Backward);
-                        IMAGE_HEIGHT
                     } else {
                         self.setup_next_animation(VideoReelDirection::Forward);
-                        -IMAGE_HEIGHT
                     };
-
-                    let (_, mut current_image) = self.get_active_images_containers();
-                    Self::set_vertical_position(&mut current_image, initial_offset, cx);
                     self.reset_images_visibility();
 
+                    self.offset_from_drag = -self.offset;
                     self.animator_play(cx, id!(carrousel.restart));
                 }
+                self.dragging = false;
             },
             _ => {}
         }
@@ -252,6 +294,7 @@ impl VideoReel {
         self.previous_image_index = self.current_image_index;
         match direction {
             VideoReelDirection::Forward => {
+                println!("setup_next_animation forward");
                 self.current_image_index = (self.current_image_index + 1).rem_euclid(self.image_containers.len() as i32);
             },
             VideoReelDirection::Backward => {
